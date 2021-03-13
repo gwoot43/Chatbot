9964f3413525d12b64986a3d1d3e676ce5c7d19c
gwoot43 <77466931+gwoot43@users.noreply.github.com>
2021-03-13 23:28:16 +1100

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -5,7 +5,8 @@ cipher = Fernet(key)
 
 
 #Symmetric Encryption
-def SymmetricEncyption (): 
+def SymmetricEncryption (): 
+    #the whole encryption/decryption process
     #Encryption
     print ('In order to encrypt a message, I will first generate a random secret key!')
     time.sleep(1)
@@ -26,18 +27,51 @@ def SymmetricEncyption ():
     byte_key = bytes(secret_key,'utf-8')
     print("Decrypting...")
     time.sleep(1)
-    if byte_key == key:
+    if byte_key != key:
+        print ('Decryption unsuccessful. Secret key is invalid')
+        time.sleep(1)
+        #second chance to write the correct secret key
+        attempt_two = input ('Please try another secret key: ')
+        byte_two = bytes(attempt_two, 'utf-8')
+        if byte_two == key:
+            decrypted_msg = Fernet(byte_two).decrypt(encrypted_msg)
+            print('Decrypting...')
+            time.sleep(1)
+            print ('Decryption successful. The original message is:', decrypted_msg.decode())
+        else: 
+            print('Decryption unsuccessful again. Have a nice day!')
+    else:
         decrypted_msg = Fernet(byte_key).decrypt(encrypted_msg)
         print ('Decryption successful. The original message is:', decrypted_msg.decode())
-    else:
-        print ('Decryption unsuccessful. Secret key is invalid')
+      
+def SymmEncryptor ():
+    #Just the encryption process so people can send messages to each other usuing the decryptor 
+    print('Generating a secret key. Please hold')
+    time.sleep(1)
+    print('This will be our secret key:', key.decode())
+    time.sleep(1)
+    message = input ("Insert a message you'd like to encrypt: ").encode()
+    print('Encrypting...')
+    time.sleep(1)
+    encrypted_msg = cipher.encrypt(message)
+    print('Send this ciphertext [',encrypted_msg.decode(),'] along with your secret key to a friend so that they can decrypt your message using the decryptor!')
+    time.sleep(1)
 
-        
-    
-#def SymmetricDecryption ():
-    #given a key, use that key to decrypt the message, - fun activitiy 
-    #secret_key = input ('In order to decrypt a message, please insert the secret key:')
+
+def SymmDecryptor ():
+    #Just the decryption process so people can send messages to each other using the encryptor
+    ciphertext = input ('Copy and paste the encrypted message: ')
+    time.sleep(1)
+    print('Saving...')
+    encrypted_msg = bytes(ciphertext, 'utf-8')
+    time.sleep(1)
+    secret_key = input ('Input the shared secret key to decrypt the file: ')
+    byte_key = bytes(secret_key, 'utf-8')
+    time.sleep(1)
+    print('Decrypting...')
+    decrypted_msg = Fernet(byte_key).decrypt(encrypted_msg)
+    print('Here is the decrypted message: ', decrypted_msg.decode())
 
 
 #run
-SymmetricEncyption ()
+SymmDecryptor ()
